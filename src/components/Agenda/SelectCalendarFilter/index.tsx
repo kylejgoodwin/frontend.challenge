f3e22cc5c3e2625ec3e665f7971db9a813b53b0f
Kyle Goodwin <hi@kyle-goodwin.com>
2021-09-19 11:32:28 -0700

    :lipstick: Normalize options UI styles and improve text on filter selection

--- src/components/Agenda/SelectCalendarFilter/index.tsx
+++ src/components/Agenda/SelectCalendarFilter/index.tsx
@@ -17,7 +17,7 @@ const SelectCalendarFilter = ({ options, value, setValue }: Props): ReactElement
 
     return (
     <select className={style.select} name="calendar" id="calendar" value={value} onChange={handleChange} >
-        <option key='default-option' value=''>{value ? "Clear Selection" : "Select a Calendar"}</option>
+        <option key='default-option' value=''>{value ? "Clear Selection" : "Select a Calendar to Filter by"}</option>
         {options.map(option => <option style={{color: option.color }} key={option.id} value={option.id}>{option.color}</option>)}
     </select>)
 
